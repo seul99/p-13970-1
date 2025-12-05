86a2effc8bf1498bcf248cf2c825471f3b7e61a6
sia <piao516@naver.com>
2025-12-05 11:16:41 +0900

    feat:5강

---
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -0,0 +1,33 @@
+package com.back.domain.post.post.controller;
+
+import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@Controller
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
+public class ApiV1PostController {
+    private final PostService postService;
+
+//    @GetMapping("/api/v1/posts")
+//    @ResponseBody
+    @GetMapping
+    public List<Post> getItems() {
+        List<Post> items = postService.findAll();
+
+        return items;
+    }
+
+    @GetMapping("/{id}")
+    public Post getItem(@PathVariable int id) {
+        Post post = postService.findById(id).get();
+
+        return post;
+    }
+}
--- src/main/java/com/back/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/back/domain/post/post/repository/PostRepository.java
@@ -1,50 +1,7 @@
-package com.back.domain.post.post.service;
+package com.back.domain.post.post.repository;
 
 import com.back.domain.post.post.entity.Post;
-import com.back.domain.post.post.repository.PostRepository;
-import com.back.domain.post.postComment.entity.PostComment;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class PostService {
-    private final PostRepository postRepository;
-
-    public long count() {
-        return postRepository.count();
-    }
-
-    public Post write(String title, String content) {
-        Post post = new Post(title, content);
-
-        return postRepository.save(post);
-    }
-
-    public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
-    }
-
-    public List<Post> findAll() {
-        return postRepository.findAll();
-    }
-
-    public void modify(Post post, String title, String content) {
-        post.modify(title, content);
-    }
-
-    public void writeComment(Post post, String content) {
-        post.addComment(content);
-    }
-
-    public boolean deleteComment(Post post, PostComment postComment) {
-        return post.deleteComment(postComment);
-    }
-
-    public void modifyComment(PostComment postComment, String content) {
-        postComment.modify(content);
-    }
+public interface PostRepository extends JpaRepository<Post, Integer> {
 }
